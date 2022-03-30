1c819e4a66b1cc05a7c1314c4724363f01e90f9e
saipraveen99 <chsaipraveen33@gmail.com>
2022-03-30 10:57:14 +0000

    first commit

---
+++ IdeaProjects/shopping-cart-app/shopping-cart-app/src/test/java/handson/practice/shoppingcartapp/ShoppingCartAppApplicationTests.java
@@ -0,0 +1,13 @@
+package handson.practice.shoppingcartapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
